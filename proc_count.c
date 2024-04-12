932e7a40d2100fb8d1d241ec4db5cad896c5e8f4
Justin Truong <jt11103@g.ucla.edu>
2024-04-12 00:05:11 -0700

    Fixed error

--- proc_count.c
+++ proc_count.c
@@ -8,13 +8,13 @@ static struct proc_dir_entry *entry;
 
 static int proc_count(struct seq_file *m, void *v){
 	int count = 0;
-	struct task_strut *task;
+	struct task_struct *task;
 
 	for_each_process(task) {
 		count++;
 	}
 
-	seq_printf(m, "%d/n", count);
+	seq_printf(m, "%d\n", count);
 
 	return 0;
 }
